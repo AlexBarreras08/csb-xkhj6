5c8df6658f8658e8688b981787cb65272a80102f
Code Sandbox <hello@codesandbox.io>
2022-02-07 05:48:53 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.5ef3fb8a053562aaa54144ac524e1605.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "038831e5ae0669a0f7f3",
+    "url": "https://xale08.github.io/csb-xkhj6/static/js/main.038831e5.chunk.js"
+  },
+  {
+    "revision": "e223dc56f679c8a4fd5b",
+    "url": "https://xale08.github.io/csb-xkhj6/static/js/1.e223dc56.chunk.js"
+  },
+  {
+    "revision": "8c7044fbf1278687deb6",
+    "url": "https://xale08.github.io/csb-xkhj6/static/js/runtime~main.8c7044fb.js"
+  },
+  {
+    "revision": "371b85b7faae60c0dbb4c7ba12055ee0",
+    "url": "https://xale08.github.io/csb-xkhj6/index.html"
+  }
+];
